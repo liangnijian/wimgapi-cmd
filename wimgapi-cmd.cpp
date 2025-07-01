2c495ab1ba17aa3039728899ee154f14ee7b4679
liangnijian <3190776046@qq.com>
2025-07-01 09:14:02 +0800

    Update wimgapi-cmd.cpp

--- wimgapi-cmd.cpp
+++ wimgapi-cmd.cpp
@@ -1,9 +1,56 @@
 #include <Windows.h>
+#include <iostream>
 #include <stdio.h>
 #include <strsafe.h>
 
-// 定义必要的WIM消息类型和常量
-constexpr auto WIM_MSG_PROGRESS = 0x00000002;
+enum
+{
+    WIM_MSG = WM_APP + 0x1476,
+    WIM_MSG_TEXT,
+    WIM_MSG_PROGRESS,
+    WIM_MSG_PROCESS,
+    WIM_MSG_SCANNING,
+    WIM_MSG_SETRANGE,
+    WIM_MSG_SETPOS,
+    WIM_MSG_STEPIT,
+    WIM_MSG_COMPRESS,
+    WIM_MSG_ERROR,
+    WIM_MSG_ALIGNMENT,
+    WIM_MSG_RETRY,
+    WIM_MSG_SPLIT,
+    WIM_MSG_FILEINFO,
+    WIM_MSG_INFO,
+    WIM_MSG_WARNING,
+    WIM_MSG_CHK_PROCESS,
+    WIM_MSG_WARNING_OBJECTID,
+    WIM_MSG_STALE_MOUNT_DIR,
+    WIM_MSG_STALE_MOUNT_FILE,
+    WIM_MSG_MOUNT_CLEANUP_PROGRESS,
+    WIM_MSG_CLEANUP_SCANNING_DRIVE,
+    WIM_MSG_IMAGE_ALREADY_MOUNTED,
+    WIM_MSG_CLEANUP_UNMOUNTING_IMAGE,
+    WIM_MSG_QUERY_ABORT,
+    WIM_MSG_IO_RANGE_START_REQUEST_LOOP,
+    WIM_MSG_IO_RANGE_END_REQUEST_LOOP,
+    WIM_MSG_IO_RANGE_REQUEST,
+    WIM_MSG_IO_RANGE_RELEASE,
+    WIM_MSG_VERIFY_PROGRESS,
+    WIM_MSG_COPY_BUFFER,
+    WIM_MSG_METADATA_EXCLUDE,
+    WIM_MSG_GET_APPLY_ROOT,
+    WIM_MSG_MDPAD,
+    WIM_MSG_STEPNAME,
+    WIM_MSG_PERFILE_COMPRESS,
+    WIM_MSG_CHECK_CI_EA_PREREQUISITE_NOT_MET,
+    WIM_MSG_JOURNALING_ENABLED
+};
+
+// WIM消息回调返回码
+#define WIM_MSG_SUCCESS          ERROR_SUCCESS
+#define WIM_MSG_DONE             0xFFFFFFF0
+#define WIM_MSG_SKIP_ERROR       0xFFFFFFFE
+#define WIM_MSG_ABORT_IMAGE      0xFFFFFFFF
+
 typedef DWORD(WINAPI* WIM_MSG_CALLBACK)(
     DWORD  dwMessageId,
     WPARAM wParam,
@@ -44,13 +91,13 @@ typedef BOOL(WINAPI* WIMGetImageInformationPtr)(
 
 typedef DWORD(WINAPI* WIMRegisterMessageCallbackPtr)(
     _In_opt_ HANDLE hWim,
-    _In_     WIM_MSG_CALLBACK fpMessageProc,
+    _In_    FARPROC fpMessageProc,
     _In_opt_ PVOID  pvUserData
     );
 
 typedef DWORD(WINAPI* WIMUnregisterMessageCallbackPtr)(
     _In_opt_ HANDLE hWim,
-    _In_     WIM_MSG_CALLBACK fpMessageProc
+    _In_     FARPROC fpMessageProc
     );
 
 typedef BOOL(WINAPI* WIMCloseHandlePtr)(
@@ -158,6 +205,7 @@ void SafePrintf(LPCWSTR format, ...) {
 
 // 进度回调函数
 DWORD WINAPI ProgressCallback(DWORD dwMessageId, WPARAM wParam, LPARAM lParam, PVOID pvUserData) {
+    //std::cout << dwMessageId << std::endl;
     if (dwMessageId == WIM_MSG_PROGRESS) {
         DWORD dwPercent = (DWORD)wParam;
         SafePrintf(L"Installing... %lu%%\r", dwPercent);
@@ -189,9 +237,9 @@ BOOL InstallImage(LPCWSTR wimPath, DWORD imageIndex, LPCWSTR installPath) {
     }
 
     // 修复：注册全局进度回调函数（将第一个参数改为NULL）
-    pfnWIMRegisterMessageCallback(NULL, ProgressCallback, NULL);
+    pfnWIMRegisterMessageCallback(hWim, (FARPROC)ProgressCallback, NULL);
 
-    if (pfnWIMApplyImage(hImage, installPath, 0)) {
+    if (pfnWIMApplyImage(hImage, installPath, 0x00000100)) {
         SafePrintf(L"\nImage applied successfully\n");
         bResult = TRUE;
     }
@@ -200,7 +248,7 @@ BOOL InstallImage(LPCWSTR wimPath, DWORD imageIndex, LPCWSTR installPath) {
     }
 
     // 注销全局进度回调函数（将第一个参数改为NULL）
-    pfnWIMUnregisterMessageCallback(NULL, ProgressCallback);
+    pfnWIMUnregisterMessageCallback(NULL, (FARPROC)ProgressCallback);
 
     pfnWIMCloseHandle(hImage);
 
